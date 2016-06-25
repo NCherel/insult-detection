a7f42d1f5c2212c1ac2afd6415b96fc990fe32d5
Maskani Filali Mohamed <maskani.mohamed@gmail.com>
2016-06-25 20:31:46 +0200

    WIP TFIDF vectorizer

--- text_mining/tfidfvectorizer.py
+++ text_mining/tfidfvectorizer.py
@@ -6,7 +6,8 @@ The tokenizer is responsible for transforming the text into an array of string
 Authors : Mohamed, Hugo, Nicolas
 
 """
-
+import numpy as np
+import scipy.sparse
 
 class TfidfVectorizer():
     """ Transform a count matrice to a tf-idf matrice
@@ -21,5 +22,22 @@ class TfidfVectorizer():
     def transform(self, X):
         """ Return the tf-idf matrice taking count sparse matrice as X
         """
-        pass
+        result = np.ndarray(X.shape)
+        X = X.todense()
+        idf = np.squeeze(self.idf(X))
+        print(idf.shape)
+        idf = np.squeeze(idf, axis=0)
+        print(idf.shape)
+        for j in range(idf.shape[1]):
+            result[:,j] = X[:,j] * idf[j]
+     
+        return result
     
+    def idf(self, X):
+        n_samples, n_terms = X.shape
+        n_t = np.array(n_terms)
+        
+        # No to sparse matrix        
+        n_t = np.sum(X > 0, axis=0)
+        
+        return np.log(n_samples/n_t)
